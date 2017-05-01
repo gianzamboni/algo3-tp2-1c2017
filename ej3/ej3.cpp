c411fc793e9510a04e94ef17323f809805e00032
Jazmin <jazminalvarezvico@gmail.com>
2017-05-01 18:16:18 -0300

    ej3 terminado

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -5,19 +5,58 @@
 
 using namespace std;
 
-struct Ruta{
+
+
+struct ruta{
 	int c1;
 	int c2;
 	int costo;
-	Ruta(){}
-	Ruta(int ciu1, int ciu2, int cost) : 
+	ruta(){}
+	ruta(int ciu1, int ciu2, int cost) : 
 		c1(ciu1),c2(ciu2),costo(cost)
 		{};
 };
 
-bool comp(Ruta r1, Ruta r2){
+bool comp(ruta r1, ruta r2){
 	return r1.costo < r2.costo;
 }
+
+//declaracion funciones
+void kruskal(unsigned int n, vector<ruta> rutas);
+
+//funciones
+
+void kruskal(unsigned int n, vector<ruta> rutas){
+	int res=0;	
+	vector<ruta> solu;
+	dsu conj(n);
+	for (int i = 0; i < rutas.size(); ++i){
+		ruta r=rutas[i];		
+		int c1=r.c1;
+		int c2=r.c2;
+		int costo= r.costo;
+		if(conj.find(c1) != conj.find(c2)){				
+			if(costo>0){
+				res=res+costo;
+			}	
+			solu.push_back(r);
+			conj.unir(c1,c2);
+		}
+		else{
+			if(costo<0){
+				res= res+ abs(costo);
+			}
+		}
+	}
+
+	cout<< res << " "<< solu.size()<< " ";
+	for(int i=0; i<solu.size(); i++){
+		ruta actual= solu[i];
+		cout<< actual.c1<<" "<<actual.c2<< " ";
+	}
+	cout << endl;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -25,36 +64,20 @@ int main(int argc, char const *argv[])
 	cin >> n;
 	while (n != -1){
 		int lineas = (n*(n-1))/2;
-		vector<Ruta> v(lineas);
+		vector<ruta> v(lineas);
 		for (int i = 0; i < lineas; ++i){
 			int c1, c2, e, p;
 			cin >> c1 >> c2 >> e >> p;
 			if(e)
-				v[i] = Ruta(c1, c2, -p);
+				v[i] = ruta(c1, c2, -p);
 			else
-				v[i] = Ruta(c1, c2, p);
+				v[i] = ruta(c1, c2, p);
 		}
 
 		sort(v.begin(), v.end(), comp);
-		//cout << llamada(n, v);
+		kruskal(n, v);
 		cin >> n;
 	}
 	return 0;
 }
 
-void kruskal(unsigned int n,vect<ruta> rutas){
-	int res=0;	
-	dsu conj(n);
-	while(!rutas.empty()){
-		ruta r=ruta.top();
-		ruta.pop();
-		int c1=r.c1();
-		int c2=r.c2();
-		if(conj.find(c1) != conj.find(c2)){
-			int costo= abs(ruta.costo());
-			res=res+costo;
-			conj.unir(c1,c2);
-		}
-	}
-
-}
