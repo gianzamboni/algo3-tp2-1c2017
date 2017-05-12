328106e88e3cdf8afc80eabf74e1c2f6bdf46d3f
Gaston Fourcade <gaston.fdm@gmail.com>
2017-05-12 17:30:21 -0300

    tester ej3, toma de tiempos funcionando

--- tester_ej3.py
+++ tester_ej3.py
@@ -6,20 +6,31 @@ from timeit import default_timer as timer
 ''' Generating Tests and Executing '''
 N = 30	# from B to N size
 Z = 20	# how many times each size
+R = 20  # how many times each case
 
 open("res3", 'w').close()
+csvEj3 = open("./timesEj2.csv", "a")
 
-for z in range(Z):
-    print(z)
-    file = str(N) + ' '
-    for n in range(1,N):
-        for x in range(n+1,N+1):
-            ls = ''
-            ls += str(n) + ' ' + str(x) 
-            ls += ' ' + str(randrange(2)) + ' ' + str(randrange(50))
-            file += ls + ' '
-    file += str(-1)
-    
-    comm = "echo %s | ./ej3 >> res3" % (file)
-    os.system(comm)
+for n in range(1,N):
+    for z in range(Z):
+        print(z)
+        file = str(N) + ' '
+        for n in range(1,N):
+            for x in range(n+1,N+1):
+                ls = ''
+                ls += str(n) + ' ' + str(x) 
+                ls += ' ' + str(randrange(2)) + ' ' + str(randrange(50))
+                file += ls + ' '
+        file += str(-1)
+        
+        comm = "echo %s | ./ej3 >> res3" % (file)
+        for i in range(0,R):
+            startEj = timer()
+            os.system(comm)
+            endEj = timer()
+            timeEj = endEj - startEj
+            csvEj3.write('%s, %s, %s\n'%(str(N),str(timeEj),ls))
 
+
+
+csvEj3.close()
